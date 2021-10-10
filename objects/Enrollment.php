32ff0d399ff29a6a14b776690910f38037d4ef55
nwc200 <wcng.2019@sis.smu.edu.sg>
2021-10-10 12:19:13 +0800

    Spacing

--- objects/Enrollment.php
+++ objects/Enrollment.php
@@ -9,41 +9,49 @@ class Enrollment
     private $Completed;
     private $User; //User Class (Aggregate)
 
-        public function __construct($EnrollmentID, $EnrolmentStatus, $SelfEnrol, $DateTimeEnrolled, $Course, $Completed, $User){
-            $this->EnrollmentID = $EnrollmentID;
-            $this->EnrolmentStatus = $EnrolmentStatus;
-            $this->SelfEnrol = $SelfEnrol;
-            $this->DateTimeEnrolled = $DateTimeEnrolled;
-            $this->Course = $Course;
-            $this->Completed = $Completed;
-            $this->User = $User;
-        }
-
-        public function getEnrollmentID(){
-            return $this->EnrollmentID;
-        }
-
-        public function getEnrolmentStatus(){
-            return $this->EnrolmentStatus;
-        }
-
-        public function getSelfEnrol(){
-            return $this->SelfEnrol;
-        }
-
-        public function getDateTimeEnrolled(){ 
-            return $this->DateTimeEnrolled;
-        }
-        public function getCourseID(){ 
-            return $this->Course;
-        }
-        public function getCompleted(){ 
-            return $this->Completed;
-        }
-        public function getUserName(){ 
-            return $this->User;
-        }
-
+    public function __construct($EnrollmentID, $EnrolmentStatus, $SelfEnrol, $DateTimeEnrolled, $Course, $Completed, $User)
+    {
+        $this->EnrollmentID = $EnrollmentID;
+        $this->EnrolmentStatus = $EnrolmentStatus;
+        $this->SelfEnrol = $SelfEnrol;
+        $this->DateTimeEnrolled = $DateTimeEnrolled;
+        $this->Course = $Course;
+        $this->Completed = $Completed;
+        $this->User = $User;
     }
+
+    public function getEnrollmentID()
+    {
+        return $this->EnrollmentID;
+    }
+
+    public function getEnrolmentStatus()
+    {
+        return $this->EnrolmentStatus;
+    }
+
+    public function getSelfEnrol()
+    {
+        return $this->SelfEnrol;
+    }
+
+    public function getDateTimeEnrolled()
+    {
+        return $this->DateTimeEnrolled;
+    }
+    public function getCourseID()
+    {
+        return $this->Course;
+    }
+    public function getCompleted()
+    {
+        return $this->Completed;
+    }
+    public function getUserName()
+    {
+        return $this->User;
+    }
+
+}
 
 ?>
