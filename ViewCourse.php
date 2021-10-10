efa348f8fe59ecfa61c1043c129972c0446bd404
XIHWEE WEE <xihwee.wee.2019@scis.smu.edu.sg>
2021-10-11 01:08:26 +0800

    Added filter button to link to ViewCourseByEligibility.php

--- ViewCourse.php
+++ ViewCourse.php
@@ -42,6 +42,9 @@
                     <div class="col-auto">
                         <button type="submit" class="btn btn-primary mb-3">Search</button>
                     </div>
+                    <div class="col-auto">
+                        <a href="ViewCourseByEligibility.php" button type="filter" class="btn btn-secondary mb-3">Filter</a>
+                    </div>
                 </div>
 
                 <?php
