d139d3a8833d56a3c0299e67ab65d7e0093d8605
XIHWEE WEE <xihwee.wee.2019@scis.smu.edu.sg>
2021-11-04 02:30:40 +0800

    Tdd for Course Class

--- tests/TestCourseSection.php
+++ tests/TestCourseSection.php
@@ -16,38 +16,67 @@ require 'objects/User.php';
 class TestCourseSection extends \PHPUnit\Framework\TestCase
 {
     public function testAddCoursePrereq()
-    {   $course = new Course (5, "Intro to programming", "This course aims to teach the programming");
+    {
         // courseID, CourseName, CourseDescription
+        $course = new Course (5, "Intro to programming", "This course aims to teach the programming");
         $course->addCoursePrereq(1);
         $course->addCoursePrereq(3);
         $course = $course->getCoursePrereq();
         $counter = count($course);
-        $this->assertSame(2, $counter); //Should return true...
+        $this->assertSame(2, $counter);
         
     }
 
-    public function testAddCourse()
+    public function testgetCourse()
     {
        
         $course = new Course (5, "Intro to programming", "This course aims to teach the programming");
         $course = $course->getCourseName();
         $this->assertEquals("Intro to programming", $course);
         
+    }
+
+    public function testgetCoursePrereq()
+    {
+        $course = new Course (5, "Programming for Xerox WorkCenter with Card Access .", "This course aims to teach the fundamentals of programming for Xerox WorkCentre.");
+        $course->getCoursePrereq(4);
+        $prereqResult = 4;
+        $this->assertSame(4, $prereqResult);
+
+        
+    }
+    public function testgetCourseID()
+    {
+        $course = new Course (1, "Fundamentals of Xerox WorkCentre 7845", "This course aims to teach the functionalities of Xerox WorkCentre 7845.");
+        $eligibility = $course->getCourseID(1);
+
+        $this->assertEquals(1, $eligibility);
 
         
     }
 
-    //  public function testGetCourse()
-    // {
+    public function testAddCourseClass()
+    {
         
+        $course = new Course (5, "Programming for Xerox WorkCenter with Card Access .", "This course aims to teach the fundamentals of programming for Xerox WorkCentre.");
+        //$ClassID, $CourseID, $ClassSize, $TrainerUserName, $StartDate, $EndDate, $StartTime, $EndTime, $SelfEnrollmentStart, $SelfEnrollmentEnd
+        $course->addClass1(1, 5, 3, "Wei Cheng", "2021-11-30", "2021-11-30", "08:00:00", "17:00:00", "2021-11-15", "2021-11-30");
+        $course->addClass1(2, 5, 5, "Alexandra", "2021-11-25", "2021-11-25", "08:00:00", "17:00:00", "2021-11-15", "2021-11-25");
         
-    // }
+        $getClass1 = $course->getClass1();
+        $counter = count($getClass1);
+        $this->assertSame(2, $counter);
+        
+       
+    }
 
 
 
 
-    
+
 }
+
+
 ?>
 
 <!-- php vendor/bin/phpunit tests/TestCourseSection.php -->
