7a8c31ead430987490579c5dc0d9904ea6a25092
yuhaoN <73381738+yuhaoN@users.noreply.github.com>
2021-11-05 01:22:06 +0800

    update enrolment.sql v3

--- sql/enrolment.sql
+++ sql/enrolment.sql
@@ -29,7 +29,7 @@ insert into enrolment
 values (1, "Approved", false, "2021-11-22 00:00:01", 1, 1, false, "Yu Hao");
 
 insert into enrolment
-values (2, "Approved", false, "2021-03-22 00:00:01", 3, 2, false, "Yu Hao");
+values (2, "Approved", false, "2021-03-22 00:00:01", 3, 5, false, "Yu Hao");
 
 insert into enrolment
 values (3, "Approved", true, "2021-03-22 00:00:01", 1, 1, false, "Mei Lan");
@@ -38,4 +38,4 @@ insert into enrolment
 values (4, "Pending", true, "", 2, 1, false, "Kai Hao");
 
 insert into enrolment
-values (5, "Pending", true, "", 3, 2, false, "Mei Xin");
+values (5, "Pending", true, "", 3, 5, false, "Mei Xin");
